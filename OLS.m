59ba507e09fca81262203958113e96b08b2a8a84
Jessica Johnston <jessica.johnston@yale.edu>
2020-03-10 14:41:24 -0500

    Delete file

--- OLS.m
+++
@@ -1,9 +0,0 @@
-function [a b rsqr resid] = OLS(x,y)
-
-x1 = [ones(length(x),1) x];
-linefit = inv(x1'*x1)*x1'*y;
-resid = y - x*linefit(2) - linefit(1); 
-ym = y - mean(y); 
-rsqr = 1 - ( resid'*resid)/(ym'*ym);
-a = linefit(2);
-b = linefit(1);
